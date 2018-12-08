82aef38d16bd80d429c23eff210e574740642df5
Cedricium <amaya.cedric@gmail.com>
2018-12-07 21:07:58 -0800

    :bug: link footer text to personal site

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,9 @@ class Footer extends Component {
     return (
       <footer className="Footer">
         <div>
-          <p>An experiment by Ceddy Spaghetti Studios</p>
+          <p>
+            An experiment by <a href="https://cedric.tech">Ceddy Spaghetti Studios</a>
+          </p>
         </div>
       </footer>
     )
